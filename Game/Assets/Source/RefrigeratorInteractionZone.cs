eac2d0c74ecdfaa5b66c039ec85d28321e8d594f
Ankur Banerjee <rukna2003@gmail.com>
2026-09-11 09:53:14 +0530

    Add Refrigerator Open and Close Interaction Mechanic

--- Game/Assets/Source/RefrigeratorInteractionZone.cs
+++ Game/Assets/Source/RefrigeratorInteractionZone.cs
@@ -1,9 +1,52 @@
+using TMPro;
 using UnityEngine;
 
 public class RefrigeratorInteractionZone : MonoBehaviour {
+
+    public TMP_Text interact_open_text;
+    public TMP_Text interact_close_text;
+
+    public bool is_refrigerator_open;
+
+    void Start() {
+        is_refrigerator_open = false;
+    }
     
     private void OnTriggerEnter(Collider other) {
         Debug.Log("Player Entered");
+
+        interact_open_text.gameObject.SetActive(true);
+    }
+
+    private void OnTriggerExit(Collider other) {
+        Debug.Log("Player Exited!");
+
+        interact_open_text.gameObject.SetActive(false);
+        interact_close_text.gameObject.SetActive(false);
+
+        is_refrigerator_open = false;
+    }
+    void process_fridge_state() {
+        if (is_refrigerator_open) {
+            interact_open_text.gameObject.SetActive(false);
+            interact_close_text.gameObject.SetActive(true);
+        }
+        else {
+            interact_open_text.gameObject.SetActive(true);
+            interact_close_text.gameObject.SetActive(false);
+        }
+    }
+
+    void process_input() {
+        if (Input.GetKeyDown(KeyCode.E)) {
+            is_refrigerator_open = !is_refrigerator_open;
+            process_fridge_state();
+        }
+    }
+
+
+    void Update() {
+        process_input();
     }
 
 }
