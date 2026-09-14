44ca496b55ef66592061b90b144e7a3231163311
Ankur Banerjee <rukna2003@gmail.com>
2026-09-15 02:08:55 +0530

    Refactor refrigerator interaction zone source

--- Game/Assets/Source/RefrigeratorInteractionZone.cs
+++ Game/Assets/Source/RefrigeratorInteractionZone.cs
@@ -2,82 +2,91 @@ using TMPro;
 using UnityEngine;
 
 public class RefrigeratorInteractionZone : MonoBehaviour {
-
+    
     public TMP_Text interact_open_text;
     public TMP_Text interact_close_text;
     public GameObject ingredient_options;
 
+    
     public bool is_refrigerator_open;
 
+    
     public bool is_inside_interaction_zone;
-
     public Hand player_hand;
 
+    
     public GameObject[] ingredients;
 
     void Start() {
         is_refrigerator_open = false;
-
         is_inside_interaction_zone = false;
-    }
-    
-    private void OnTriggerEnter(Collider other) {
-        interact_open_text.gameObject.SetActive(true);
 
-        is_inside_interaction_zone = true;
+        update_ui();
     }
 
-    private void OnTriggerExit(Collider other) {
-        interact_open_text.gameObject.SetActive(false);
-        interact_close_text.gameObject.SetActive(false);
+    void Update() {
+        if (!is_inside_interaction_zone) return;
 
-        ingredient_options.SetActive(false);
-
-        is_refrigerator_open = false;
-        
-        is_inside_interaction_zone = false;
-    }
-
-    void process_fridge_state() {
-        if (is_refrigerator_open) {
-            interact_open_text.gameObject.SetActive(false);
-            interact_close_text.gameObject.SetActive(true);
-        }
-        else {
-            interact_open_text.gameObject.SetActive(true);
-            interact_close_text.gameObject.SetActive(false);
-        }
+        process_input();
     }
 
     void process_input() {
         if (Input.GetKeyDown(KeyCode.E)) {
             is_refrigerator_open = !is_refrigerator_open;
-            process_fridge_state();
+            update_ui();
+        }
+
+        if (is_refrigerator_open) {
+            process_ingredient_selection();
         }
     }
 
-    void process_ingredient_options() {
-        if (is_refrigerator_open) {
-            ingredient_options.SetActive(true);
+    void process_ingredient_selection() {
+        if (Input.GetKeyDown(KeyCode.Alpha1)) {
+            select_ingredient(0);
+        } else if (Input.GetKeyDown(KeyCode.Alpha2)) {
+            select_ingredient(1);
+        } else if (Input.GetKeyDown(KeyCode.Alpha3)) {
+            select_ingredient(2);
+        }
+    }
 
-            if (Input.GetKeyDown(KeyCode.Alpha1)) {
-                player_hand.hold_ingredient(ingredients[0]);
-            }
-            else if (Input.GetKeyDown(KeyCode.Alpha2)) {
-                player_hand.hold_ingredient(ingredients[1]);
-            }
-            else if (Input.GetKeyDown(KeyCode.Alpha3)) {
-                player_hand.hold_ingredient(ingredients[2]);
-            }
+    void select_ingredient(int index) {
+        if (index >= ingredients.Length) return;
+
+        player_hand.hold_ingredient(ingredients[index]);
+    }
+
+    void update_ui() {
+        if (!is_inside_interaction_zone) {
+            interact_open_text.gameObject.SetActive(false);
+            interact_close_text.gameObject.SetActive(false);
+            ingredient_options.SetActive(false);
+
+            return;
+        }
+
+        if (is_refrigerator_open) {
+            interact_open_text.gameObject.SetActive(false);
+            interact_close_text.gameObject.SetActive(true);
+            ingredient_options.SetActive(true);
         } else {
+            interact_open_text.gameObject.SetActive(true);
+            interact_close_text.gameObject.SetActive(false);
             ingredient_options.SetActive(false);
         }
     }
 
-    void Update() {
-        if (is_inside_interaction_zone) {
-            process_input();
-            process_ingredient_options();
-        }
+    void OnTriggerEnter(Collider other) {
+        is_inside_interaction_zone = true;
+
+        update_ui();
+    }
+
+    void OnTriggerExit(Collider other) {
+        is_inside_interaction_zone = false;
+        is_refrigerator_open = false;
+
+        update_ui();
     }
 }
