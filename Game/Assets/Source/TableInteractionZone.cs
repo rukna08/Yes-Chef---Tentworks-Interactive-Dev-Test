ae9080d9123731abfe8a838b48761ddc8041f56e
Ankur Banerjee <rukna2003@gmail.com>
2026-09-14 16:44:54 +0530

    Add table functionality 1/4

--- Game/Assets/Source/TableInteractionZone.cs
+++ Game/Assets/Source/TableInteractionZone.cs
@@ -1,7 +1,44 @@
+using TMPro;
 using UnityEngine;
 
 public class TableInteractionZone : MonoBehaviour {
-    
 
+    bool is_inside_interaction_zone;
+
+    public TMP_Text chop_vegetable_text;
+
+    public Hand player_hand;
+
+    void Start() {
+        is_inside_interaction_zone = false;
+    }
+
+    void OnTriggerEnter(Collider other) {
+        is_inside_interaction_zone = true;
+    }
+
+    void OnTriggerExit(Collider other) {
+        is_inside_interaction_zone = false;
+    }
+
+    void Update() {
+
+        if (is_inside_interaction_zone) {
+            chop_vegetable_text.gameObject.SetActive(true);
+
+
+            if (Input.GetKeyDown(KeyCode.E)) {
+                if (player_hand.held_ingredient != null && player_hand.held_ingredient.tag == "Vegetable") {
+                    Debug.Log("Chopping Vegetable");
+                    player_hand.release_ingredient();
+                }
+            }
+            
+        } else {
+            chop_vegetable_text.gameObject.SetActive(false);
+        }
+
+
+    }
 
 }
