4eba4dab410f1d783e25933cc5116ff4daa3d5ea
Ankur Banerjee <rukna2003@gmail.com>
2026-09-15 03:31:18 +0530

    Add both meat placement on stove, one by one

--- Game/Assets/Source/StoveInteractionZone.cs
+++ Game/Assets/Source/StoveInteractionZone.cs
@@ -5,7 +5,12 @@ public class StoveInteractionZone : MonoBehaviour {
 
     bool is_inside_interaction_zone;
 
-    public TMP_Text cook_meet_text;
+    public TMP_Text cook_meat_text;
+
+    public Hand player_hand;
+
+    public GameObject meat_1;
+    public GameObject meat_2;
 
     void OnTriggerEnter(Collider other) {
         is_inside_interaction_zone = true;
@@ -17,9 +22,29 @@ public class StoveInteractionZone : MonoBehaviour {
 
     void Update() {
         if(is_inside_interaction_zone) {
-            cook_meet_text.gameObject.SetActive(true);
+            cook_meat_text.gameObject.SetActive(true);
+
+            if(Input.GetKeyDown(KeyCode.E)) {
+                    
+                if (player_hand.held_ingredient != null && player_hand.held_ingredient.tag == "Meat") {
+                
+                    if(player_hand.held_ingredient != null && !meat_1.activeSelf) {
+                        meat_1.SetActive(true);
+                        player_hand.release_ingredient();
+                    }
+
+                    if(player_hand.held_ingredient != null && !meat_2.activeSelf) {
+                        meat_2.SetActive(true);  
+                        player_hand.release_ingredient();
+                    }
+                
+                }
+
+            }
+
+
         } else {
-            cook_meet_text.gameObject.SetActive(false);        
+            cook_meat_text.gameObject.SetActive(false);        
         }
     }
 }
