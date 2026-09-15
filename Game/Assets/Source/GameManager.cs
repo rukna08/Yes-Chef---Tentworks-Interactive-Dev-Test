00bd416bc12f93e52bf4678a536be82adbf146a6
Ankur Banerjee <rukna2003@gmail.com>
2026-09-15 23:35:11 +0530

    Add logs

--- Game/Assets/Source/GameManager.cs
+++ Game/Assets/Source/GameManager.cs
@@ -1,18 +1,50 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour {
     
     public int main_score;
-
+    public int high_score;
     public TMP_Text score_ui;
+    float game_runtime;
+    public GameObject game_over_screen;
+    bool is_game_over;
 
     void Start() {
         main_score = 0;
+        game_runtime = 0;
+        game_over_screen.SetActive(false);
+        is_game_over = false;
     }
 
     void Update() {
-        score_ui.text = "SCORE: " + main_score.ToString();
+        if(!is_game_over) {
+            score_ui.text = "SCORE: " + main_score.ToString();
+            process_game_runtime();
+            process_highscore();
+        }
     }
 
+    void process_highscore() {
+        if (main_score > high_score) {
+            high_score = main_score;    
+        }
+    }
+
+    void process_game_runtime() {
+        game_runtime += Time.deltaTime;
+        if(game_runtime >= 180f) {
+            game_over_screen.SetActive(true);
+            is_game_over = true;
+        }
+    }
+
+    public void restart_game() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void exit_game() {
+        Application.Quit();
+    }
 }
